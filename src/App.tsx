cb1be1f9020a918825a40cdfe2a0843c9b6ed828
emam al mounqer <emam.anagreh@gmail.com>
2022-06-21 05:10:22 +0300

    fix layout bug

--- src/App.tsx
+++ src/App.tsx
@@ -180,23 +180,22 @@ function App() {
           <div className="grid grid-cols-3 items-center mb-4 ">
             <div className="flex gap-1 items-center">
               <Button onClick={handleCurrTimer}>{isRunning ? "STOP" : "START"}</Button>
-              {currLabPassingTime > 0 && (
-                <button className="text-secondary h-6 " onClick={resetPassingTime}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"
-                    />
-                    <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z" />
-                  </svg>
-                </button>
-              )}
+
+              <button className="text-secondary h-6 " onClick={resetPassingTime}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"
+                  />
+                  <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z" />
+                </svg>
+              </button>
             </div>
 
             <div className="col-start-2 row-start-2 md:row-start-auto">
